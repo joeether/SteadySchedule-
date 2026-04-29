122eb63148205b177ffaf950dc56935a0d63ace2
joeether <37514028+joeether@users.noreply.github.com>
2026-04-29 17:15:58 -0500

    Create ViewModels/CompanyViewModel.cs
    created Company View Model

---
+++ SteadySchedule/ViewModels/CompanyViewModel.cs
@@ -0,0 +1,8 @@
+public class CompanyViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string AdminEmail { get; set; }
+    public bool IsActive { get; set; }
+    public int EmployeeCount { get; set; }
+}
