f26599e2360dce96592a6691c5073ef5cf8fa635
joeether <37514028+joeether@users.noreply.github.com>
2026-05-03 16:38:56 -0500

    Update CompanyViewModel.cs
    added trialdate

--- SteadySchedule/ViewModels/CompanyViewModel.cs
+++ SteadySchedule/ViewModels/CompanyViewModel.cs
@@ -5,6 +5,9 @@ namespace SteadySchedule.ViewModels
         public int Id { get; set; }
         public string Name { get; set; }
         public string AdminEmail { get; set; }
+        public DateTime TrialEndDate { get; set; }
+public int BillingAnchorDay { get; set; }
+public DateTime NextBillingDate { get; set; }
         public bool IsActive { get; set; }
         public int EmployeeCount { get; set; }
     }
